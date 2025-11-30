5fdb888b33e5283102c9c0acc5566687f846f43a
Richard He <103625533+Richardwarriors@users.noreply.github.com>
2025-11-30 11:15:28 -0500

    Update music recommendation system output format

--- LLM4Music/LLM4Rec.py
+++ LLM4Music/LLM4Rec.py
@@ -3,13 +3,14 @@ import os, socket
 print("Host:", socket.gethostname())
 print("CWD:", os.getcwd())
 
-SYSTEM = """You are the master music recommender system.
+SYSTEM = """You are the master music recommender system. Output format: a python list. Do not explain the reason or include any other words
 For example:
 Example1: 
-
+The user with id 15 listened Cliff Richard 1970s|Stop This Flight|16 Smash Hits by The Monkees|After The Gold Rush: The Dawn/Pye Anthology 1973-77 by Prelude (2006-09-25)|Remembering The Sixties, 1964|What You Need|The Country Store Collection|An aching and a longing [Vinyl-LP]|Preachers From Outer Space by Daniel Amos (Audio CD)|Eric Clapton: Give Me Strength: The 1974/1975 Studio Recordings and based on his or her listened music history, the top 10 recommended item is in the following: Mt. Pleasant|Your Hit Parade - 1946|A New Sunrise|Popemobile|Getting Down - 20 Blues Classics - by Lowell Fulson (Audio CD)|Stress Why You Put Me Through It|THE Great Soap Opera Themes the New Christy Minstrels [ 8 Track Stereo Tape]|MARIA LUISA LANDIN LA HORA AZUL|Living in a Bubble|Sleeping with the Past / Reg Strikes Back
 Example2: 
-
+The user with id 51 listened Running Out of Time|Classic Metal|Underground Metal 2|Tourniquet, Vol. 2|Nobody Special|Underground Metal|Rock N Race|End of the Age: The Best of Bride|Keep the Fire Burning / The World of You and I 45 rpm|Voice and based on his or her listened music history, the top 10 recommended item is in the following: Blu Blowin|60 EXITOS DE TRIO CALAVERAS|RUDOLF FIRKUSNY- SMETANA- CZECH POLKAS AND DANCES|15 EXITOS|Songs About Sex and Depression|Cumbias Con Arpa|Janus|Heyday|Orquesta Reve & Neno Gonzalez : Mano a Mano|Apples of Gold
 Example3: 
+The user with id 43 listened The Road to Canso|Aoi Ie De Bokura Kurasu|Sangre Mia|Face aux Elements Dechaines by Etron Fou Leloublan|Holy Smokes|Tricot - Kabuku Ep [Japan CD] XQMZ-1001|still a Sigure virgin?|Live! Skipper's Smokehouse 8/15/03 & 9/7/03|Culture Shock|Zyacalanda and based on his or her listened music history, the top 10 recommended item is in the following: The Great British Psychedelic Trip, Vol. 3|The Best of Sam Cooke|I'M MOVIN ON|Johann Strauss, Jr. ~ Readers Digest Favorites From the Classics|The Swingle Singers Compilation Album (Reflections & Live at Ronnie Scotts)|Laura Smith|Billy Lamont Meets Chuck Edwards|Bear Tracks: Special Bear Exports|Ferruccio Busoni - Berceuse Elegiaque op 42, Canto della Ronda degli Spirit op 47, Lustspielouverture op 38, Four Songs, Violin Concerto in D op 35a, Rondo Arlecchinesco op 46|Scherrer/Fritz: Symphonies & Violin Concerto
 """
 
 def query_ollama_chat(model, prompt_question, max_token = 2048):
@@ -29,11 +30,12 @@ def query_ollama_chat(model, prompt_question, max_token = 2048):
     data = r.json()
     return (data.get("message") or {}).get("content","").strip()
 
-with open("input file", encoding="utf-8") as f, open("output results","w",encoding="utf-8") as w:
+with open("../data/Music/prompt_ADM.txt", encoding="utf-8") as f, open("Output_result.txt","w",encoding="utf-8") as w:
     for line in f:
-        t = line.strip() + "Output format: a python list. Do not explain the reason or include any other words"
+        t = line.strip()
 
         g = query_ollama_chat('gemma3:4b',t)
         w.write(f"{g}\n")
         print(f"{t} => {g}")
+
 
